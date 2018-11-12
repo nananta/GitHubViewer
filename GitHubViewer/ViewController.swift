288f8c1dd34f436f798e2863b8b648cb6048d5fa
nananta <tananta@me.com>
2018-11-12 12:08:30 -0800

    Initial Commit

---
+++ GitHubViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubViewer
+//
+//  Created by Nitinan Ananta on 11/12/18.
+//  Copyright © 2018 Nitinan Ananta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
